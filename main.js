e1ec304438df780e1b881014b220a46b9ef599c6
Emka0000 <131450137+Emka0000@users.noreply.github.com>
2023-10-13 01:32:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let gallery = document.querySelector("#Sport .flex")
+for (let i = 0; i < gallery.children.length; i++) {
+    gallery.children[i].addEventListener("click", (e) => {
+        if (e.currentTarget.classList.contains("expanded") == false) {
+            console.log(e.currentTarget)
+            for (let j = 0; j < gallery.children.length; j++)
+                gallery.children[j].classList.remove("expanded")
+            e.currentTarget.classList.add("expanded")
+        } else {
+            e.currentTarget.classList.remove("expanded")
+        }
+    })
+}
+document.addEventListener("scroll", (e) => {
+    for (let j = 0; j < gallery.children.length; j++)
+        gallery.children[j].classList.remove("expanded")
+    document.querySelector(":root").style.setProperty('--window-scroll-y', `${window.scrollY}px`)
+})
